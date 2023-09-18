8a6859a70c884f347e5123e38007a4e6225c0d54
Kateattardo <youremail@domain.com>
2023-09-18 19:16:51 -0400

    date picker

--- main_app/forms.py
+++ main_app/forms.py
@@ -1,23 +1,25 @@
+from django import forms
 from django.forms.widgets import DateTimeInput
 from django import ModelForm
 from main_app.models import, Project, Task, Comment
 
 class ProjectForm(forms.ModelForm):
-  date_field_name = forms.DateTimeField(widget=CustomDateTimeInput())
+  due_date = forms.DateTimeField(widget=CustomDateTimeInput())
 
   class Meta:
-    model = project
+    model = Project
     fields = [...]
 
-class TaskForm(forms.modelForm):
-  date_field_name = forms. DateTimeField(widget=CustomDateTimeInput())
+class TaskForm(forms.ModelForm):
+  created_date = forms. DateTimeField(widget=CustomDateTimeInput()),
+  due_date = forms.DateTimeField(widget=CustomDateTimeInput())
 
   class Meta:
     model = Task
     fields = [...]
 
 class CommentForm(forms.ModelForm):
-  date_field_name = forms.DateTimeField(widget=CustomDateTimeInput())
+  created_date = forms.DateTimeField(widget=CustomDateTimeInput())
 
   class Meta:
     model = Comment
