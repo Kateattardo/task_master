16d77814d82e5cf4ccad377070a18cc2f538db02
Kateattardo <youremail@domain.com>
2023-09-18 20:43:30 -0400

    date forms

--- main_app/forms.py
+++ main_app/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.forms.widgets import DateTimeInput
 from django import ModelForm
-from main_app.models import, Project, Task, Comment
+from main_app.models import, Project, Task, 
 
 class ProjectForm(forms.ModelForm):
   due_date = forms.DateTimeField(widget=CustomDateTimeInput())
@@ -11,16 +11,10 @@ class ProjectForm(forms.ModelForm):
     fields = [...]
 
 class TaskForm(forms.ModelForm):
-  created_date = forms. DateTimeField(widget=CustomDateTimeInput()),
   due_date = forms.DateTimeField(widget=CustomDateTimeInput())
 
   class Meta:
     model = Task
     fields = [...]
 
-class CommentForm(forms.ModelForm):
-  created_date = forms.DateTimeField(widget=CustomDateTimeInput())
 
-  class Meta:
-    model = Comment
-    fields = [...]
